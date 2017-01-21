3269b5039e689c4fc3e354adc734e55a3779c048
antham <hamonanth@gmail.com>
2017-01-22 00:01:07 +0100

    Rewrite test

--- env_tree_test.go
+++ env_tree_test.go
@@ -12,36 +12,16 @@ func TestCreateTreeFromDelimiterFilteringByRegexp(t *testing.T) {
 
 	n, err := createTreeFromDelimiterFilteringByRegexp(regexp.MustCompile("ENVH"), "_")
 
-	nodes := n.findAllChildsByKey("TEST3", true)
-
 	assert.NoError(t, err, "Must return no errors")
-	assert.Len(t, *nodes, 1, "Must contains 1 element")
-	assert.Equal(t, "test1", (*nodes)[0].value, "Must have correct value")
 
-	nodes = n.findAllChildsByKey("TEST4", true)
+	for key, expected := range map[string]string{"TEST3": "test1", "TEST4": "test2", "TEST6": "test3", "TEST1": "test5", "TEST2": "test4"} {
+		nodes := n.findAllChildsByKey(key, true)
 
-	assert.Len(t, *nodes, 1, "Must contains 1 element")
-	assert.Equal(t, "test2", (*nodes)[0].value, "Must have correct value")
+		assert.Len(t, *nodes, 1, "Must contains 1 element")
+		assert.Equal(t, expected, (*nodes)[0].value, "Must have correct value")
+	}
 
-	nodes = n.findAllChildsByKey("TEST6", true)
-
-	assert.Len(t, *nodes, 1, "Must contains 1 element")
-	assert.Equal(t, "test3", (*nodes)[0].value, "Must have correct value")
-
-	nodes = n.findAllChildsByKey("TEST1", true)
-
-	assert.Len(t, *nodes, 1, "Must contains 1 element")
-	assert.Equal(t, "test5", (*nodes)[0].value, "Must have correct value")
-
-	// Find all childs with a value defined
-	nodes = n.findAllChildsByKey("TEST2", true)
-
-	assert.Len(t, *nodes, 1, "Must contains 1 element")
-
-	assert.Equal(t, "test4", (*nodes)[0].value, "Must have correct value")
-
-	// Find all childs with values or not
-	nodes = n.findAllChildsByKey("TEST2", false)
+	nodes := n.findAllChildsByKey("TEST2", false)
 
 	assert.Len(t, *nodes, 2, "Must contains 2 elements")
 }
